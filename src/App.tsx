4824e9c8cda787b3ce63df47c2d1f1d3c2463fbb
Yuheng Chen <ccyyhh97@hotmail.com>
2021-11-11 19:54:16 -0600

    merge problem

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import './App.css';
+import PriorityQueue from 'priorityqueuejs'
+
 import OrderBook from './views/OrderBook';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -8,59 +10,149 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Button from 'react-bootstrap/Button';
 
+import io from 'socket.io-client';
+
 const socket = new WebSocket("wss://www.cryptofacilities.com/ws/v1")
+// const END_POINT = "wss://www.cryptofacilities.com/ws/v1"
 
 function App() {
   const [connected, setConnected] = useState(false)
-  const [data, setData] = useState<{} | null>(null)
+
   const [feed, setFeed] = useState<{} | null>(null)
   const [productID, setProductID] = useState<string>("PI_XBTUSD")
 
-  // const [message, setMessage] = useState()
+  const [message, setMessage] = useState()
 
-  // const [spread, setSpread] = useState<number>()
+  // const [socket, setSocket] = useState(null);
+
+  // const [bidsPQ, setBidsPQ] = useState<PriorityQueue<number[]>>()
+  // const [asksPQ, setAsksPQ] = useState<PriorityQueue<number[]>>()
+
+  const [bidsArray, setBidsArray] = useState<number[][]>([])
+  const [asksArray, setAsksArray] = useState<number[][]>([])
+
 
   useEffect(() => {
     socket.onopen = () => {
-      // console.log('connection established')
-      // setConnected(true)
+      console.log('connection established')
+      setConnected(true)
 
-      socket.send('{"event":"subscribe","feed":"book_ui_1","product_ids":["PI_XBTUSD"]}')
+      socket.send(generateMessage("subscribe", "PI_XBTUSD"))
 
       socket.onmessage = (event) => {
-        // const data = JSON.parse(event.data)
-        const data = event.data;
-        console.log(data)
+        const data = JSON.parse(event.data)
 
-        setData(data)
-      } 
+        // console.log(data)
+
+        const ifEvent = data.hasOwnProperty('event')
+
+        if (ifEvent) return // Info messages, we ignore them
+
+        if (data.hasOwnProperty('numLevels')) { // snapshot messages
+          initialize(data.numLevels, data.bids, data.asks)
+          return
+        }
+
+        // then we have the atcual delta messages
+        else {
+          setBidsArray(prevState => {
+            return [...prevState, ...data.bids]
+            // .sort((a: number[], b: number[]) => {
+            //   return a[0] - b[0]
+            // })
+
+          });
+          setAsksArray(prevState => {
+            return merge(prevState, data.asks)
+            // return [...prevState, ...data.asks]
+          });
+        }
+      }
     }
-  }, [setConnected, setData])
+  }, [connected, asksArray, bidsArray])
 
-  const communicationStart = () => {
+  const initialize = (numLevels: number, bids: [], asks: []) => {
 
+    console.log("Init")
+
+    bids.sort((a, b) => {
+      return a[0] - b[0]
+    })
+
+    asks.sort((a, b) => {
+      return b[0] - a[0]
+    })
+
+    setBidsArray(bids);
+    setAsksArray(asks);
+
+    console.log(bidsArray)
+    // console.log(asksArray)
+  }
+
+  const update = (bids: number[][], asks: number[][]) => {
+
+
+  }
+
+  // merge the incoming quotes with existing list (need to ensure matching order)
+  // assuming both arry in ascending order
+  const merge = (current: number[][], incoming: number[][]) => {
+    console.log("attempting to merge", current, incoming)
+
+    let merged: number[][] = []
+    let c = 0 // pointer for current data
+    let i = 0 // pointer for incoming data
+
+    while (c < current.length && i < incoming.length) {
+      console.log("fu")
+      if (current[c][0] > incoming[i][0]) {
+        if (incoming[i][1] != 0) merged.push(incoming[i])
+        i++
+      }
+      else if (current[c][0] === incoming[i][0]) {
+        if (incoming[i][1] != 0) merged.push(incoming[i])
+        c++
+        i++
+      }
+      else if (current[c][0] < incoming[i][0]) {
+        merged.push(current[c])
+        c++
+      }
+    }
+    console.log("merged", merged);
+
+    let res:number[][] = [];
+
+    if (c < current.length) {
+      res = merged.concat(current.slice(c))
+      // current.slice(c).forEach((e)=>{merged.push(e)})
+    }
+
+    if (i < incoming.length) {
+      res = merged.concat(incoming.slice(c))
+    }
+    console.log(res)
+
+    return res
   }
 
   const stop = () => {
     // socket.send(generateMessage("unsubscribe", productID))
     socket.close()
+    setConnected(false)
   }
 
   const asks = () => {
-    // let entries: JSX.Element[] = []
-    // let entries: string[] = []
-    // if (data) {
-    //   data?.asks?.forEach(element => {
-    //     entries.push(`Price: ${element[0]}, Size: ${element[1]}`)
-    //   });
-    // }
-    // return entries
+
   }
 
   const spread = () => {
-    const highestBid = 100
-    const lowestAsk = 110
-    return lowestAsk - highestBid
+    if (bidsArray && asksArray) {
+      // const highestBid = bidsArray[0][0]
+      // const lowestAsk = asksArray[0][0]
+      // return lowestAsk - highestBid
+    }
   }
 
   // const total
@@ -68,14 +160,16 @@ function App() {
 
 
   const toggleFeed = () => {
-    socket.send(generateMessage("unsubscribe", productID))
-    if (productID === "PI_XBTUSD") {
-      socket.send(generateMessage("subscribe", "PI_ETHUSD"))
-      setProductID("PI_ETHUSD")
-    }
-    else if (productID === "PI_ETHUSD") {
-      socket.send(generateMessage("subscribe", "PI_XBTUSD"))
-      setProductID("PI_XBTUSD")
+    if (connected) {
+      socket.send(generateMessage("unsubscribe", productID))
+      if (productID === "PI_XBTUSD") {
+        socket.send(generateMessage("subscribe", "PI_ETHUSD"))
+        setProductID("PI_ETHUSD")
+      }
+      else if (productID === "PI_ETHUSD") {
+        socket.send(generateMessage("subscribe", "PI_XBTUSD"))
+        setProductID("PI_XBTUSD")
+      }
     }
   }
 
@@ -98,22 +192,61 @@ function App() {
 
         <Container>
           <Row className="header">
-            <Col>Spread: {13} ({12.5}%)</Col>
+            <Col>Spread: {spread()}</Col>
           </Row>
           <Row className="toggleField ">
             <Col>Current Feed: {productID}</Col>
             <Col><Button onClick={toggleFeed}>Toggle Feed</Button></Col>
           </Row>
           <Row className="bookTable">
-            <Col className="ask">
-              <Row>ask</Row>
-              {/* {asks()} */}
-              {/* {data?.feed} */}
-              {data}
+            <Col className="bid" md={6}>
+              <Row>BID</Row>
+              <Row>
+                <Col>
+                  <Row>PRICE</Row>
+                  {/* {bidsArray?.map((e) =>
+                    <Row key={e[0]}>{e[0]}</Row>)
+                  } */}
+                </Col>
+                <Col>
+                  <Row>SIZE</Row>
+                  {/* {bidsArray?.map((e) =>
+                    <Row key={e[0]}>{e[1]}</Row>)
+                  } */}
+                </Col>
+                <Col>
+                  <Row>TOTAL</Row>
+                  {/* {bidsArray?.map((e) =>
+                    <Row key={e[0]}>{e[2]}</Row>)
+                  } */}
+                </Col>
+              </Row>
             </Col>
-            <Col className="bid">
-              <Row>bid</Row>
+            <Col className="ask" md={6}>
+              <Row>ASK</Row>
+              <Row>
+                <Col>
+                  <Row>PRICE</Row>
+                  {asksArray?.map((e) =>
+                    <Row key={e[0]}>{e[0]}</Row>)
+                  }
+                </Col>
+                <Col>
+                  <Row>SIZE</Row>
+                  {asksArray?.map((e) =>
+                    <Row key={e[0]}>{e[1]}</Row>)
+                  }
+                </Col>
+                <Col>
+                  <Row>TOTAL</Row>
+                  {/* {asksArray?.map((e) =>
+                    <Row key={e[0]}>{e[2]}</Row>)
+                  } */}
+                </Col>
+              </Row>
+
             </Col>
+
           </Row>
 
         </Container>
